b07f545797b232b013465ff9bb1471c631de3beb
Rhoney Seabra <rhoneyholanda@gmail.com>
2023-03-31 13:12:08 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let mn = dateToday.getMinutes();
+    let sc = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+    
+    if (mn < 10) mn = '0' + mn;
+    
+    if (sc < 10) sc = '0' + sc;
+    
+    horas.textContent = hr;
+    minutos.textContent = mn;
+    segundos.textContent = sc;
+})
+
+let dataAtual = document.querySelector("js-data");
+
+let data = new Date();
+
+const objData = {
+    year: 'numeric',
+    month: 'long',
+    weekday: 'long',
+    day: 'numeric',
+}
+
+dataAtual.textContent = data.toLocaleTimeString("pt-BR", objData);
